97ea6f3d97af596c9b14be12f439309f43433018
GabrielAceti <gabriel.aceti@hotmail.com>
2021-06-30 17:15:34 -0300

    Creating configuration of the connection with the database

---
+++ src/data/Config.ts
@@ -0,0 +1,13 @@
+var config = {
+    server: "192.168.0.15",
+    options: {},
+    authentication: {
+      type: "default",
+      options: {  
+        userName: "sa",
+        password: "M@$t3rk3yRM",
+      }
+    }
+  };
+
+export default config;
