888579f585ab307d571ae5d17c7dfe622d49313d
GabrielAceti <gabriel.aceti@hotmail.com>
2021-06-30 17:21:22 -0300

    Creating costumer interface

---
+++ src/app/models/Costumer.ts
@@ -0,0 +1,10 @@
+export default interface CostumerInterface{
+    date: Date;
+    name: String,
+    fantasyName: String,
+    address?: String;
+    telephone?: String;
+    observation?: String;
+    type: String;
+}
+
