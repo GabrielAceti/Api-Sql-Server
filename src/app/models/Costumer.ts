ec5b6f07526bcbfd285bee9398c6a190375fa303
GabrielAceti <gabriel.aceti@hotmail.com>
2021-07-05 17:03:06 -0300

    Recreating Costumer.js as a class

--- src/app/models/Costumer.ts
+++ src/app/models/Costumer.ts
@@ -1,10 +1,22 @@
-export default interface CostumerInterface{
-    date: Date;
-    name: String,
-    fantasyName: String,
-    address?: String;
-    telephone?: String;
-    observation?: String;
-    type: String;
+export default class Costumer{
+
+    private date: Date;
+    private name: String;
+    private fantasyName: String;
+    private address: String;
+    private telephone: String;
+    private observation: String;
+    private type: String;
+
+    constructor(date: Date, name: String, fantasyName: String, address: String, telephone: String, observation: String, type: String){
+        this.date = date;
+        this.name = name;
+        this.fantasyName = fantasyName;
+        this.address = address;
+        this.telephone = telephone;
+        this.observation = observation;
+        this.type = type;
+    }
+
 }
 
